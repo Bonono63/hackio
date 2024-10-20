c43589281be6b84e0a21375d69225d6221ec1ba2
Bonono63 <bonoria63@gmail.com>
2024-10-19 20:38:15 -0400

    Initial commit

---
+++ client/client.py
@@ -0,0 +1,37 @@
+import cv2
+import socket
+import pickle
+import struct
+
+print("HackI/O video client")
+print("Running CV2 verison: ", cv2.__version__)
+
+video_capture = cv2.VideoCapture(0)
+print("video capture started")
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Find a way to read these from a file or something
+ip = '192.168.1.203'
+port = 567
+# create the socket in the OS
+server_socket.bind(('127.0.0.1', port))
+server_socket.listen(10)
+print("socket bound on 127.0.0.1 on port {port} and listening")
+
+# connect to client
+client_socket, client_address = server_socket.accept()
+print(f"[*] Accepted connection from {client_address}")
+
+# read until it recieves a certain output
+while True:
+    ret, frame = video_capture.read()
+    serialized_frame = pickle.dumps(frame)
+    message_size = struct.pack("L", len(serialized_frame))
+    client_socket.sendall(message_size + serialized_frame)
+    cv2.imshow('Server Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# release resources
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ server/server.py
@@ -0,0 +1,44 @@
+import cv2
+import socket
+import pickle
+import struct
+
+print("HackI/O video server")
+print("Running CV2 verison: ", cv2.__version__)
+
+
+connection_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Find a way to read these from a file or something
+ip = '192.168.1.203'
+port = 567
+# create the socket in the OS
+connection_socket.connect(('127.0.0.1', port))
+
+received_data = b""
+payload_size = struct.calcsize("L")
+
+# read until it recieves a certain output
+while True:
+    while len(received_data) < payload_size:
+        received_data += connection_socket.recv(4096)
+
+    packed_msg_size = received_data[:payload_size]
+    received_data = received_data[payload_size:]
+    msg_size = struct.unpack("L", packed_msg_size)[0]
+    
+    while len(received_data) < msg_size:
+        received_data += connection_socket.recv(4096)
+
+    frame_data = received_data[:msg_size]
+    received_data = received_data[msg_size:]
+
+    received_frame = pickle.loads(frame_data)
+
+    cv2.imshow('Client Video', received_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# release resources
+cv2.destroyAllWindows()
+connection_socket.close()

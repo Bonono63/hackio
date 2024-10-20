657d8124cddbb4c1cb47ef2b0fff5e90ad921c03
Max McGee <maxemcgee@gmail.com>
2024-10-19 22:59:51 -0400

    Added GPIO test

---
+++ server/gpioTest.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set up GPIO PIN
+signal_pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(signal_pin,GPIO.OUT)
+
+# Set up pulse width modulation
+pwm = GPIO.PWM(signal_pin, 50)
+pwm.start(7.5)
+
+try:
+    while True:
+        pwm.ChangeDutyCycle(5)
+        time.sleep(1)
+        pwm.ChangeDutyCycle(10)
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+
+pwm.stop()
+GPIO.cleanup()
